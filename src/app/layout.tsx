849a54f5f7cb7e7b5aa82d7c06c59deb5ccaae7d
Dominik Kanjuh <109925257+DominikKanjuh@users.noreply.github.com>
2025-11-17 20:01:44 +0100

    add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,21 @@
 import MovingIconBackground from "@/components/background/moving-icon-background";
+import { cn } from "@/lib/utils";
 import "@styles/globals.css";
+import { Albert_Sans, Fira_Code } from "next/font/google";
+
+const albertSans = Albert_Sans({
+  subsets: ["latin"],
+  style: ["normal", "italic"],
+  variable: "--font-albert-sans",
+  display: "swap",
+});
+
+const firaCode = Fira_Code({
+  subsets: ["latin"],
+  style: ["normal"],
+  variable: "--font-fira-code",
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -7,8 +23,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={"antialiased"}>
+    <html lang="hr" dir="ltr">
+      <body
+        className={cn(
+          "font-sans antialiased",
+          albertSans.variable,
+          firaCode.variable,
+        )}
+      >
         <MovingIconBackground />
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <span className="font-serif">Serif</span>
+      <span className="font-sans">Sans</span>
+      <span className="font-mono">Mono</span>
+    </div>
+  );
 }
