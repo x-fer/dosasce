1859993520c34d8e91852a33235992ffb8590651
Dominik Kanjuh <109925257+DominikKanjuh@users.noreply.github.com>
2025-11-23 20:21:14 +0100

    fix build

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,6 +1,8 @@
 import { createClient } from "@/lib/supabase/server";
 import { NextResponse } from "next/server";
 
+export const runtime = "edge";
+
 export async function GET(request: Request) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
