a990c95ae65250fd9c16c494255b1c2e82e859f5
Dominik Kanjuh <109925257+DominikKanjuh@users.noreply.github.com>
2025-11-29 18:45:58 +0100

    diffrenet login with user category

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -14,15 +14,16 @@ export async function GET(request: Request) {
     const { error } = await supabase.auth.exchangeCodeForSession(code);
 
     if (!error) {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      const userCategory = user?.user_metadata?.user_category;
+
+      const redirectPath = userCategory ? next : "/settings";
       const forwardedHost = request.headers.get("x-forwarded-host");
-      const isLocalEnv = process.env.NODE_ENV === "development";
-      if (isLocalEnv) {
-        return NextResponse.redirect(`${origin}${next}`);
-      } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`);
-      } else {
-        return NextResponse.redirect(`${origin}${next}`);
-      }
+      const redirectBase = forwardedHost ? `https://${forwardedHost}` : origin;
+
+      return NextResponse.redirect(`${redirectBase}${redirectPath}`);
     }
   }
 
